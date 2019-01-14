385e6ffb593e63240ec5e22f1d10c291267c169a
angelo-milcevski <1074282@ucn.dk>
2019-01-14 20:47:58 +0100

    V2 slider updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,11 @@
     
     function close_slider(){
          var slider =  document.getElementsByClassName("slider");
+         var slider_links =  document.getElementsByClassName("slider_links");
+        
          slider[0].style="width:0px;"
+         slider_links[0].style="display:none"
+        
 
 
     }
