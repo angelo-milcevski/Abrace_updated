099da4d2a7e6a07c02237ca7cfb332e1b174d87b
angelo-milcevski <1074282@ucn.dk>
2019-01-14 22:15:22 +0100

    chair product gallery added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
  function slide(){
       var slider =  document.getElementsByClassName("slider");
+      var slider_links =  document.getElementsByClassName("slider_links");
+
         
         slider[0].style="width:250px;"
+        slider_links[0].style="display:block"
+
     }
     
     function close_slider(){
