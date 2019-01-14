69614f53837eb93f9978489cd44ec6ec3b53b59b
angelo-milcevski <1074282@ucn.dk>
2019-01-14 20:36:11 +0100

    V2
    JS slider added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+ function slide(){
+      var slider =  document.getElementsByClassName("slider");
+        
+        slider[0].style="width:250px;"
+    }
+    
+    function close_slider(){
+         var slider =  document.getElementsByClassName("slider");
+         slider[0].style="width:0px;"
+
+
+    }
+    
+    
